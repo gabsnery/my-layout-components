9e13bd84a4575f9220d2cbdc7dd4247ba9f3f52e
Gabriela Nery <gneri94@gmail.com>
2024-06-29 22:54:13 -0300

    ci: test release

---
+++ src/components/Button2/styles.ts
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+
+export default StyledButton
